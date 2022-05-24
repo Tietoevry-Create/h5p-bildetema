5deb49f70eaf132c80451b55e87145c75e80fc0f
Stian Haugland <stian.haugland@tietoevry.com>
2022-05-24 09:44:15 +0200

    Update h5p-bildetema-topic-list/src/components/Word/Word.stories.tsx
    Co-authored-by: Sindre Bøyum <boyum@users.noreply.github.com>

--- h5p-bildetema-topic-list/src/components/Word/Word.stories.tsx
+++ h5p-bildetema-topic-list/src/components/Word/Word.stories.tsx
@@ -21,7 +21,7 @@ Default.args = {
   word: {
     id: "V0889",
     label: "Elg",
-    images: ["http://oddestemmen-camp.no/images/phocagallery/Tierbilder/thumbs/phoca_thumb_l__mg_2903.jpg"]
+    images: ["https://images.unsplash.com/photo-1549471013-3364d7220b75?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340"]
   },
   textVisible: true
 }
