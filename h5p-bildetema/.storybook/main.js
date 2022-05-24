31a108b7c14f5ca4841eaf90919f662f8e802d62
Sindre Bøyum <boyum@users.noreply.github.com>
2022-05-24 20:35:26 +0200

    remove unused import

--- h5p-bildetema/.storybook/main.js
+++ h5p-bildetema/.storybook/main.js
@@ -1,5 +1,3 @@
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
