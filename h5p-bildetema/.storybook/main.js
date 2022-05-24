61db0fe6522332afec3e40c74ca95b669df7c6d8
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-05-24 18:38:01 +0200

    use vite for running Storybook at greater speed

--- h5p-bildetema/.storybook/main.js
+++ h5p-bildetema/.storybook/main.js
@@ -1,37 +1,9 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   core: {
-    builder: "webpack5",
-  },
-  webpackFinal: async config => {
-    addScssSupport(config);
-    return config;
+    builder: "@storybook/builder-vite",
   },
 };
-
-function addScssSupport(config) {
-  config.plugins.push(new MiniCssExtractPlugin());
-
-  config.module.rules.push({
-    test: /\.module.scss$/,
-    use: [
-      MiniCssExtractPlugin.loader,
-      {
-        loader: "css-loader",
-        options: {
-          modules: true,
-        },
-      },
-      "sass-loader",
-    ],
-  });
-
-  config.module.rules.push({
-    test: /\.scss$/,
-    use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-    exclude: /\.module\.scss$/,
-  });
-}
