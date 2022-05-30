39a4d22a39c1e096680032cdc637c0ea7ea7d046
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-05-31 00:21:51 +0200

    use `h5p-generate-configs` cli to generate semantics

--- h5p-bildetema/src/semantics.ts
+++ h5p-bildetema/src/semantics.ts
@@ -1,3 +1,3 @@
-import { H5PBehaviour, H5PField, H5PL10n } from "h5p-types";
+import type { H5PBehaviour, H5PField, H5PL10n } from "h5p-types";
 
 export const semantics: Readonly<Array<H5PField | H5PBehaviour | H5PL10n>> = [];
