fa84bf8b978b9bf89e03982fe79c51d8a693cfcb
Andrey B <beliy255@gmail.com>
2022-06-03 12:40:19 +0200

    fix storybook

--- h5p-bildetema/src/components/App/App.stories.tsx
+++ h5p-bildetema/src/components/App/App.stories.tsx
@@ -1,10 +1,19 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import * as React from "react";
+import { HashRouter } from "react-router-dom";
+import { makeLanguageCode } from "../../../../common/utils/LanguageCode.utils";
 import App from "./App";
 
 export default {
   title: "App",
   component: App,
+  decorators: [
+    Story => (
+      <HashRouter>
+        <Story />
+      </HashRouter>
+    ),
+  ],
 } as ComponentMeta<typeof App>;
 
 const Template: ComponentStory<typeof App> = args => (
@@ -13,6 +22,13 @@ const Template: ComponentStory<typeof App> = args => (
 );
 
 export const Default = Template.bind({});
+
 Default.args = {
   adjective: "funny",
+  currentLanguage: {
+    label: "Norsk Bokmål",
+    code: makeLanguageCode("nb"),
+    rtl: false,
+    isFavorite: false,
+  },
 };
--- h5p-bildetema/src/components/Bildetema/Bildetema.stories.tsx
+++ h5p-bildetema/src/components/Bildetema/Bildetema.stories.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { ComponentMeta } from "@storybook/react";
+import { HashRouter } from "react-router-dom";
 import { Bildetema } from "./Bildetema";
+import { makeLanguageCode } from "../../../../common/utils/LanguageCode.utils";
+import { Language } from "../../../../common/types/types";
 
 export default {
   title: "Bildetema/Bildetema",
   component: Bildetema,
+  decorators: [
+    Story => (
+      <HashRouter>
+        <Story />
+      </HashRouter>
+    ),
+  ],
 } as ComponentMeta<typeof Bildetema>;
 
-export const Default = (): JSX.Element => <Bildetema />;
+export const Default = (
+  args: JSX.IntrinsicAttributes & { currentLanguage: Language },
+  // eslint-disable-next-line react/jsx-props-no-spreading
+): JSX.Element => <Bildetema {...args} />;
 
 Default.args = {
   adjective: "test",
+  currentLanguage: {
+    label: "Norsk Bokmål",
+    code: makeLanguageCode("nb"),
+    rtl: false,
+    isFavorite: false,
+  },
 };
