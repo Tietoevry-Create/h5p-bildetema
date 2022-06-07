1179c868abdfed372f6613abfc6c00830a90c207
Kent Inge F. Simonsen <40064+kentis@users.noreply.github.com>
2022-06-07 12:10:04 +0200

    Apply suggestions from code review
    Co-authored-by: Sindre Bøyum <boyum@users.noreply.github.com>

--- h5p-bildetema-words-tree-view/src/library.ts
+++ h5p-bildetema-words-tree-view/src/library.ts
@@ -1,8 +1,8 @@
 import type { Library } from "h5p-types";
 
 export const library: Library = {
-  title: "H5P Bildetema Grid View",
-  machineName: "H5P.BildetemaGridView",
+  title: "H5P Bildetema Tree View",
+  machineName: "H5P.BildetemaTreeView",
   majorVersion: 1,
   minorVersion: 0,
   patchVersion: 0,
