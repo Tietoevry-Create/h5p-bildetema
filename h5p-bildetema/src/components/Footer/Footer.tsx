d0bf14816572a3471a6cc9388e551a80c1c71bf3
benq <beliy255@gmail.com>
2022-06-08 10:11:45 +0200

    replace p with div (heading)

--- h5p-bildetema/src/components/Footer/Footer.tsx
+++ h5p-bildetema/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = (): JSX.Element => {
   return (
     <div className={styles.footer}>
       {/* TODO: Replace with the current language translation */}
-      <p className={styles.heading}>Kontaktinformasjon</p>
+      <div className={styles.heading}>Kontaktinformasjon</div>
       <div className={styles.groupedElements}>
         <a href="https://nafo.oslomet.no/">
           NAFO - Nasjonalt senter for flerkulturell opplæring
