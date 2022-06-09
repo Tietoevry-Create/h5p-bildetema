0542e6d056c1d5d4f7f5557db6bb80f6164436fb
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-06-09 10:57:19 +0200

    localize Topic Image View content type

--- h5p-bildetema-words-theme-image/src/semantics.ts
+++ h5p-bildetema-words-theme-image/src/semantics.ts
@@ -1,3 +1,18 @@
 import type { H5PBehaviour, H5PField, H5PL10n } from "h5p-types";
 
-export const semantics: Readonly<Array<H5PField | H5PBehaviour | H5PL10n>> = [];
+export const semantics: Readonly<Array<H5PField | H5PBehaviour | H5PL10n>> = [
+  {
+    name: "behaviour",
+    type: "group",
+    label: "Behavioral settings",
+    importance: "low",
+    fields: [],
+  },
+  {
+    name: "l10n",
+    type: "group",
+    common: true,
+    label: "Localize",
+    fields: [],
+  },
+];
