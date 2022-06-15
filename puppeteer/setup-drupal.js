857931132a084d03cb8944ccd4b4d514c2d018a7
Stian Haugland <stian.haugland@tietoevry.com>
2022-06-15 08:35:11 +0200

    Add github workflow to setup drupal

--- puppeteer/setup-drupal.js
+++ puppeteer/setup-drupal.js
@@ -1,14 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const url = "http://localhost:8080/"
+  const url = process.env.DRUPAL_URL ? process.env.DRUPAL_URL : 'http://localhost:8080/';
+  const title = process.env.TITLE ? process.env.TITLE : 'some title';
   
   // debug settings:
-  // const browser = await puppeteer.launch({dumpio: true, headless: true, devtools: true});
+  // const browser = await puppeteer.launch({dumpio: true, headless: false, devtools: true});
   const browser = await puppeteer.launch();
   
   const page = await browser.newPage();
-  await page.goto('http://localhost:8080');
   await page.goto(`${url}`, {
     waitUntil: 'networkidle2',
   });
@@ -18,7 +18,7 @@ const puppeteer = require('puppeteer');
   await page.click('#edit-submit')
   await page.waitForNavigation({ waitUntil: 'networkidle2' })
   
-  await page.goto(`${url}node#overlay=node/add/h5p-content`, {
+  await page.goto(`${url}/node#overlay=node/add/h5p-content`, {
     waitUntil: 'networkidle2',
   });
   
@@ -31,8 +31,9 @@ const puppeteer = require('puppeteer');
   const h5pBildetemaEditorIframe = await page.mainFrame().childFrames()[1].childFrames()[0];
   const editorIframe = await page.mainFrame().childFrames()[1]
   
-  await h5pBildetemaEditorIframe.type('#field-extratitle--1', 'test')
+  await h5pBildetemaEditorIframe.type('#field-extratitle--1', title)
   await editorIframe.click('#edit-submit')
+  await page.waitForNavigation({ waitUntil: 'networkidle2' })
   
   await browser.close();
 })();
