5ea2b461b02d14fd696b4a6e14d29c882798cc31
benq <beliy255@gmail.com>
2022-06-15 15:45:54 +0200

    replace temp image url to unsplash url

--- h5p-bildetema/src/components/TopicGrid/TopicGrid.stories.tsx
+++ h5p-bildetema/src/components/TopicGrid/TopicGrid.stories.tsx
@@ -15,7 +15,7 @@ const baseTopic: Topic = {
   subTopics: new Map<string, Topic>(),
   words: new Map<LanguageCode, Word[]>(),
   image: {
-    path: "https://s3-alpha-sig.figma.com/img/68c5/8247/e72b8c415b6ce9d1d6a9be597d0a4d41?Expires=1656288000&Signature=UTefYYa7istTasgM6UyM3pkW8ZKa1DMyAtBre4U71LnybnhJBQZnVegj1ar-d3XYupRijqy7~KCvtA9zCsvwjwohj-8EqElm9RyZD-R4Py3cmRCXyNOgwrYZdQHjcLatIqGQr7Zpj6X5hEBcZi1K~g2TOIbrTw6QdRJulmQMtxkb-RIydyenPKA23qJxlOH-gBwvEXdSegDtWQSosCCkohi8LzbQy6~S4m7TcsLCUmzjjK-jkOBxNwihZxfC0HXNSkZMFDHLvhEQB8UJ7L6Fb4aGWAo6AVz1cdgBBnURTTSIDrgRcF6e6uwyTNCs1l8DZbA-le52jYAAVd9KW8I~pQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+    path: "https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?ixlib=rb-1.2.1&w=640&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
   },
 };
 
