ea27500bb3c9db1f449bae94ce0a31abb41808ff
Kent Inge F. Simonsen <kent.simonsen@tietoevry.com>
2022-06-17 13:05:25 +0200

    added concept of theme and theme type

--- h5p-bildetema-words-theme-image/src/components/ThemeImageContainer/ThemeImageContainer.tsx
+++ h5p-bildetema-words-theme-image/src/components/ThemeImageContainer/ThemeImageContainer.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import { Word } from "../../../../common/types/types";
+import { ThemeImageTypes } from "../../types/ThemeImageTypes";
 import styles from "./ThemeImageContainer.module.scss";
 
 type ThemeImageContainerProps = {
+  theme: Word;
+  themeImageType: ThemeImageTypes;
   words: Word[];
 };
 
 export const ThemeImageContainer: React.FC<ThemeImageContainerProps> = ({
+  theme,
+  themeImageType,
   words,
 }) => {
   const [currentWord, setCurrentWord] = React.useState("");
@@ -17,6 +22,8 @@ export const ThemeImageContainer: React.FC<ThemeImageContainerProps> = ({
 
   return (
     <div className={styles.wrapper}>
+      {theme.label} <br />
+      {themeImageType} <br />
       {currentWord} <br />
       {words.map(word => {
         return (
---
+++ h5p-bildetema-words-theme-image/src/types/ThemeImageTypes.ts
@@ -0,0 +1,5 @@
+export type ThemeImageTypes =
+  | "vectorImageWithHotspots"
+  | "nonVectorImageWithHotspots"
+  | "3dSceneWithhotSpots"
+  | "panoramaSceneWithHotspots";
