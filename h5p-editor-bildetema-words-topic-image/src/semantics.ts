187895282d64d5f9363c244afbf42da918e9a2c4
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-06-22 11:00:15 +0200

    remove semantics

--- h5p-editor-bildetema-words-topic-image/src/semantics.ts
+++
@@ -1,9 +0,0 @@
-import type { H5PField, H5PBehaviour, H5PL10n } from "h5p-types";
-
-export const semantics: Readonly<Array<H5PField | H5PBehaviour | H5PL10n>> = [
-  {
-    name: "name",
-    type: "text",
-    label: "What is your name?",
-  },
-];
