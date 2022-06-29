02f55f6bfe39a33008c29049baf08ddc8d41581b
Stian Haugland <stian.haugland@tietoevry.com>
2022-06-29 16:14:11 +0200

    init editor

---
+++ h5p-editor-bildetema-words-topic-image/src/components/Image/Image.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import * as React from "react";
+import { Image } from "./Image";
+
+export default {
+  title: "Components/Image",
+  component: Image,
+} as ComponentMeta<typeof Image>;
+
+const Template: ComponentStory<typeof Image> = args => (
+  /* eslint-disable-next-line react/jsx-props-no-spreading */
+  <Image {...args}/>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  image: {
+    path: "https://images.unsplash.com/photo-1617051571090-85766fa13621?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+    width: 800
+  }
+};
---
+++ h5p-editor-bildetema-words-topic-image/src/components/editor/Editor.stories.tsx
@@ -0,0 +1,33 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import * as React from "react";
+import { Editor } from "./Editor";
+
+export default {
+  title: "Components/Editor",
+  component: Editor,
+} as ComponentMeta<typeof Editor>;
+
+const Template: ComponentStory<typeof Editor> = args => (
+  /* eslint-disable-next-line react/jsx-props-no-spreading */
+  <Editor {...args}/>
+);
+
+export const Portrait = Template.bind({});
+Portrait.args = {
+  image: {
+    // path: "https://images.unsplash.com/photo-1537017206014-37e7e84d84c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+    path: "https://images.unsplash.com/photo-1617051571090-85766fa13621?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
+  }
+};
+export const Landscape = Template.bind({});
+Landscape.args = {
+  image: {
+    path: "https://images.unsplash.com/photo-1537017206014-37e7e84d84c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+  }
+};
+
+
+// export const EmptyBreadcrumb = Template.bind({});
+// EmptyBreadcrumb.args = {
+//   Editor: [],
+// };
