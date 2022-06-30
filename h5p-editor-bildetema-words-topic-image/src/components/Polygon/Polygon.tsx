f8a1087c35a9b2de8751c24624b52d2372093c87
Stian Haugland <stian.haugland@tietoevry.com>
2022-06-30 16:01:54 +0200

    Allow drawing multiple polygons

--- h5p-editor-bildetema-words-topic-image/src/components/Polygon/Polygon.tsx
+++ h5p-editor-bildetema-words-topic-image/src/components/Polygon/Polygon.tsx
@@ -1,22 +1,58 @@
-import React from 'react'
-import type {Hotspot} from '../Svg/Svg'
+import React from "react";
+import type { Hotspot } from "../Svg/Svg";
 
 export type PolygonProps = {
-  hotspot: Hotspot
-}
+  hotspot: Hotspot;
+};
 
-export const Polygon: React.FC<PolygonProps> = ({hotspot:{points, finishedDrawing}}) => {
-  const pointsToDAttribute = ():string=> {
-    const d = points.map(({x, y}, index) => `${index === 0 ? "M" : "L"}${x} ${y}`).join(" ")
-    return finishedDrawing ? `${d} Z` : d
-  }
-  
+export const Polygon: React.FC<PolygonProps> = ({
+  hotspot: { points, drawing },
+}) => {
+  const pointsToDAttribute = (): string => {
+    const d = points
+      .map(({ x, y }, index) => `${index === 0 ? "M" : "L"}${x} ${y}`)
+      .join(" ");
+    return drawing ? d : `${d} Z`;
+  };
+
+  const findRadius = (a: Point, b: Point): number => {
+    return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+  };
+
   return (
     <>
-      {/* {points.length && <path d="M 10 10 L 100 100" fill="transparent" stroke="black"/>} */}
-      {points.length && <path d={pointsToDAttribute()} fill="transparent" stroke="black"/>}
-      {/* {!finished && <circle cx="50" cy="50" r="1" fill="red"/>} */}
-      {!finishedDrawing && points.map(({x,y}) => (<circle key={`${x}${y}`}cx={x} cy={y} r="1" fill="red"/>))}
+      {/* {points.length && <path d={pointsToDAttribute()} fill="transparent" strokeWidth="0.3" stroke="black"/>} */}
+      {points.length && points.length === 2 && !drawing ? (
+        <circle
+          cx={points[0].x}
+          cy={points[0].y}
+          r={findRadius(points[0], points[1])}
+          stroke="black"
+          fill="none"
+          strokeWidth="0.3"
+        />
+      ) : (
+        points.length && (
+          <path
+            d={pointsToDAttribute()}
+            fill="transparent"
+            strokeWidth="0.3"
+            stroke="black"
+          />
+        )
+      )}
+      {drawing &&
+        points.map(({ x, y }) => (
+          <circle
+            key={`${x}${y}`}
+            cx={x}
+            cy={y}
+            r="0.5"
+            stroke="black"
+            fill="none"
+            strokeWidth="0.3"
+          />
+        ))}
     </>
-  )
-}
+  );
+};
--- h5p-editor-bildetema-words-topic-image/src/components/editor/Editor.stories.tsx
+++ h5p-editor-bildetema-words-topic-image/src/components/editor/Editor.stories.tsx
@@ -9,7 +9,7 @@ export default {
 
 const Template: ComponentStory<typeof Editor> = args => (
   /* eslint-disable-next-line react/jsx-props-no-spreading */
-  <Editor {...args}/>
+  <Editor {...args} />
 );
 
 export const Portrait = Template.bind({});
@@ -17,15 +17,25 @@ Portrait.args = {
   image: {
     // path: "https://images.unsplash.com/photo-1537017206014-37e7e84d84c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
     path: "https://images.unsplash.com/photo-1617051571090-85766fa13621?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80",
-  }
+  },
+  words: [
+    { audio: "", label: "Mor", id: "V0002", images: [] },
+    { audio: "", label: "Far", id: "V0003", images: [] },
+    { audio: "", label: "Sønn", id: "V0005", images: [] },
+  ],
 };
 export const Landscape = Template.bind({});
 Landscape.args = {
   image: {
-    path: "https://images.unsplash.com/photo-1537017206014-37e7e84d84c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
-  }
+    path: "https://prodbildetemabackend.blob.core.windows.net/images/xlarge/V0001a.jpeg",
+  },
+  words: [
+    { audio: "", label: "Mor", id: "V0002", images: [] },
+    { audio: "", label: "Far", id: "V0003", images: [] },
+    { audio: "", label: "Sønn", id: "V0005", images: [] },
+    { audio: "", label: "Datter", id: "V0006", images: [] },
+  ],
 };
-
 
 // export const EmptyBreadcrumb = Template.bind({});
 // EmptyBreadcrumb.args = {
