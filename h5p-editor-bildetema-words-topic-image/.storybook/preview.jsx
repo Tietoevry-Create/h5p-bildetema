2f0bbe834f7cc052d1a2c679068b93d9494831a7
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-08-03 10:01:54 +0200

    add story with initial hotspots

---
+++ h5p-editor-bildetema-words-topic-image/.storybook/preview.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "../../h5p-bildetema-words-grid-view/src";
+import "../../h5p-bildetema-words-theme-image/src";
+import "../../h5p-bildetema-words-tree-view/src";
+
+const queryClient = new QueryClient();
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (/** @type {React.FC} */ Story) => (
+    <QueryClientProvider client={queryClient}>
+      <Story />
+    </QueryClientProvider>
+  ),
+];
