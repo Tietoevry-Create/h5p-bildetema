4930a1328ba8b31c5596adf6303550848689090c
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-08-08 13:26:36 +0200

    add fake test

---
+++ h5p-bildetema-words-grid-view/test/a.test.ts
@@ -0,0 +1,7 @@
+// Vitest fails if there are zero tests. Remove this test if there are more than zero other tests.
+
+import { expect, test } from "vitest";
+
+test("Vitest works", () => {
+  expect(true).toBeTruthy();
+});
