bd1e4d7f4a8f12bcc1d2b5f65701e406c14bf7cb
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-08-09 11:38:19 +0200

    Add max width for main content (#328)
    * Add max width for main content
    
    * Bump patch version
    
    * Align header logo with content max width
    
    Move logo free space from svg to scss

--- h5p-editor-bildetema-words-topic-image/src/library.ts
+++ h5p-editor-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5PEditor.BildetemaWordsTopicImage",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 2,
+  patchVersion: 3,
   runnable: 0,
   preloadedJs: [
     {
