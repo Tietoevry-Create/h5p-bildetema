bde60182944e92d631583aeb6323cbc31dbb8007
Stian Haugland <stian.haugland@tietoevry.com>
2022-08-12 13:38:43 +0200

    prettier

--- h5p-bildetema/src/components/RouteController/RouteController.tsx
+++ h5p-bildetema/src/components/RouteController/RouteController.tsx
@@ -52,7 +52,8 @@ export const RouteController: React.FC<RouteControllerProps> = ({
       if (!word) return false;
 
       const { label, id } = word;
-      if (label !== "") return labelToUrlComponent(label) === labelToUrlComponent(topicValue);
+      if (label !== "")
+        return labelToUrlComponent(label) === labelToUrlComponent(topicValue);
 
       return labelToUrlComponent(id) === topicValue;
     });
