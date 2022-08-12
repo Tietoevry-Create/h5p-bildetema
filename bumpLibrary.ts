bf711a2f1b06a573f004493931f1521a2283fdb1
Stian Haugland <stian.haugland@tietoevry.com>
2022-08-12 16:14:19 +0200

    prettier

--- bumpLibrary.ts
+++ bumpLibrary.ts
@@ -1,52 +1,65 @@
-import {library as h5pBildetema} from "./h5p-bildetema/src/library"
-import {library as h5pBildetemaWordsGridView} from "./h5p-bildetema-words-grid-view/src/library"
-import {library as h5pBildetemaWordsTopicImage} from "./h5p-bildetema-words-topic-image/src/library"
-import {library as h5pBildetemaWordsTreeView} from "./h5p-bildetema-words-tree-view/src/library"
-import {library as h5pEditorBildetemaWordsTopicImage} from "./h5p-editor-bildetema-words-topic-image/src/library"
-import query from 'cli-interact'
+import { library as h5pBildetema } from "./h5p-bildetema/src/library";
+import { library as h5pBildetemaWordsGridView } from "./h5p-bildetema-words-grid-view/src/library";
+import { library as h5pBildetemaWordsTopicImage } from "./h5p-bildetema-words-topic-image/src/library";
+import { library as h5pBildetemaWordsTreeView } from "./h5p-bildetema-words-tree-view/src/library";
+import { library as h5pEditorBildetemaWordsTopicImage } from "./h5p-editor-bildetema-words-topic-image/src/library";
+import query from "cli-interact";
 import type { Library } from "h5p-types";
-import fs from "fs"
+import fs from "fs";
 
 type pathAndLibrary = {
-  path: string
-  lib: Library,
-}
+  path: string;
+  lib: Library;
+};
 
 const libs: pathAndLibrary[] = [
-  {path: "./h5p-bildetema/src/library.ts", lib:h5pBildetema},
-  {path: "./h5p-bildetema-words-grid-view/src/library.ts", lib:h5pBildetemaWordsGridView},
-  {path: "./h5p-bildetema-words-topic-image/src/library.ts", lib:h5pBildetemaWordsTopicImage},
-  {path: "./h5p-bildetema-words-tree-view/src/library.ts", lib:h5pBildetemaWordsTreeView},
-  {path: "./h5p-editor-bildetema-words-topic-image/src/library.ts", lib:h5pEditorBildetemaWordsTopicImage},
-] 
+  { path: "./h5p-bildetema/src/library.ts", lib: h5pBildetema },
+  {
+    path: "./h5p-bildetema-words-grid-view/src/library.ts",
+    lib: h5pBildetemaWordsGridView,
+  },
+  {
+    path: "./h5p-bildetema-words-topic-image/src/library.ts",
+    lib: h5pBildetemaWordsTopicImage,
+  },
+  {
+    path: "./h5p-bildetema-words-tree-view/src/library.ts",
+    lib: h5pBildetemaWordsTreeView,
+  },
+  {
+    path: "./h5p-editor-bildetema-words-topic-image/src/library.ts",
+    lib: h5pEditorBildetemaWordsTopicImage,
+  },
+];
 
-libs.forEach((el) => {
-  const {path, lib} = el
-  
-  const currPatchVersion = lib.patchVersion
-  const bumpedPatchVersion = currPatchVersion + 1
-  
-  const bumpPatch = query.getYesNo(`Bump patchVersion for "${lib.title}" from ${currPatchVersion} -> ${bumpedPatchVersion}`)
-  if(!bumpPatch) return
-  
+libs.forEach(el => {
+  const { path, lib } = el;
+
+  const currPatchVersion = lib.patchVersion;
+  const bumpedPatchVersion = currPatchVersion + 1;
+
+  const bumpPatch = query.getYesNo(
+    `Bump patchVersion for "${lib.title}" from ${currPatchVersion} -> ${bumpedPatchVersion}`,
+  );
+  if (!bumpPatch) return;
 
   fs.readFile(path, "utf8", (err, data) => {
-    if(err) {
-      console.log(err)
-      return
+    if (err) {
+      console.log(err);
+      return;
     }
 
-    const dataArray = data.split("\n")
+    const dataArray = data.split("\n");
 
-    const libraryFileWithBumpedPatchVersion = dataArray.map(line => {
-      if(!line.includes("patchVersion")) return line
-      return line.replace(`${currPatchVersion}`, `${bumpedPatchVersion}`)
-    }).join("\n")
-    
-    fs.writeFile(path, libraryFileWithBumpedPatchVersion, (err) => {
-      if(err) console.log(err)
-    })
-  })
+    const libraryFileWithBumpedPatchVersion = dataArray
+      .map(line => {
+        if (!line.includes("patchVersion")) return line;
+        return line.replace(`${currPatchVersion}`, `${bumpedPatchVersion}`);
+      })
+      .join("\n");
 
-})
-
+    fs.writeFile(path, libraryFileWithBumpedPatchVersion, err => {
+      if (err) console.log(err);
+    });
+  });
+});
