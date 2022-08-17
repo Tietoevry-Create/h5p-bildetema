93a695e96ec7c29651fbfced7edc686178afd2fb
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-08-17 10:37:54 +0200

    Fix width of square svg topic images (#390)

--- h5p-bildetema-words-topic-image/src/library.ts
+++ h5p-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.BildetemaTopicImageView",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 11,
+  patchVersion: 12,
   runnable: 1,
   preloadedJs: [
     {
