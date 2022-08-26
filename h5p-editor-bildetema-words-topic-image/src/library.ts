13cd27b9189a96c1032832081b1109e2e281c0dc
Kent Inge F. Simonsen <kent.simonsen@tietoevry.com>
2022-08-26 10:54:27 +0200

    bump version

--- h5p-editor-bildetema-words-topic-image/src/library.ts
+++ h5p-editor-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5PEditor.BildetemaWordsTopicImage",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 17,
+  patchVersion: 18,
   runnable: 0,
   preloadedJs: [
     {
