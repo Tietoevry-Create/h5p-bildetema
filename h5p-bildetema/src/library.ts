68965fd854c39a87d230872167afc28a465d97d8
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2022-08-29 11:18:42 +0200

    Make languageFavorites follow site margin on print
    if pages are scaled down

--- h5p-bildetema/src/library.ts
+++ h5p-bildetema/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.Bildetema",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 48,
+  patchVersion: 49,
   runnable: 1,
   preloadedJs: [
     {
