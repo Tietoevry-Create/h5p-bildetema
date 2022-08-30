16273176525828f97db3b6b66b955edde76d6019
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-08-30 16:08:04 +0200

    Fix print not loading on small screen (#457)
    * Fix print not loading on small screen
    
    * Bump patch version
    
    * Show active language always to the right on print

--- h5p-bildetema-words-grid-view/src/library.ts
+++ h5p-bildetema-words-grid-view/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.BildetemaWordsGridView",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 26,
+  patchVersion: 27,
   runnable: 1,
   preloadedJs: [
     {
