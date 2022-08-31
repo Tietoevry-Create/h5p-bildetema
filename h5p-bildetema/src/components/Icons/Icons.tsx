e27d2f1de11c045cc9080b87a1f2f0b7703379b3
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-08-31 20:34:14 +0200

    Fix language selector in Safari (#464)
    * Move transform attributes from svg to path
    
    since svg is not allowed to have transform attributes in svg1.1
    
    * Make sure languageDropdown has relative position
    
    was not positioning correctly in Safari
    
    * Bump patch version

--- h5p-bildetema/src/components/Icons/Icons.tsx
+++ h5p-bildetema/src/components/Icons/Icons.tsx
@@ -120,12 +120,12 @@ export const LanguageMenuArrowIcon: React.FC<
     viewBox="0 0 16 10"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    transform={transform}
-    transform-origin={transformOrigin}
   >
     <path
       d="M8 9.49974L0 1.49974L1.43333 0.0664062L8 6.66641L14.5667 0.0997391L16 1.53307L8 9.49974Z"
       fill="white"
+      transform={transform}
+      transform-origin={transformOrigin}
     />
   </svg>
 );
