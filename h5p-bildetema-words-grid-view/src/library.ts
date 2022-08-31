0a5e9dacf8fe7f651d6ba15aa4898429be2deeaf
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-08-31 10:05:21 +0200

    Fix z-index values (#460)
    * Fix z-index values
    
    * Bump patch version

--- h5p-bildetema-words-grid-view/src/library.ts
+++ h5p-bildetema-words-grid-view/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.BildetemaWordsGridView",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 27,
+  patchVersion: 28,
   runnable: 1,
   preloadedJs: [
     {
--- h5p-editor-bildetema-words-topic-image/src/library.ts
+++ h5p-editor-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5PEditor.BildetemaWordsTopicImage",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 21,
+  patchVersion: 22,
   runnable: 0,
   preloadedJs: [
     {
