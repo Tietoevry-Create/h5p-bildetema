f92b887cb3b0cefe11378299319a9a8ea920fcc4
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-09-01 16:35:38 +0200

    remove extraneous wrapiife

--- h5p-bildetema-words-topic-image/vite.config.ts
+++ h5p-bildetema-words-topic-image/vite.config.ts
@@ -1,10 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import react from "@vitejs/plugin-react";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "vitest/config";
-import { getBuildConfig, wrapIIFE } from "../common/utils/vite-config.utils";
+import { getBuildConfig } from "../common/utils/vite-config.utils";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react, wrapIIFE],
+  plugins: [react],
   build: getBuildConfig(),
   test: {
     environment: "jsdom",
