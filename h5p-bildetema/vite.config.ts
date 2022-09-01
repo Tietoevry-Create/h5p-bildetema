46d3bbcd883c86989e0da773dfde8c1380625eba
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-09-01 16:21:04 +0200

    remove `wrapIIFE`
    vite in `iife` mode builds a safe bundle already

--- h5p-bildetema/vite.config.ts
+++ h5p-bildetema/vite.config.ts
@@ -1,12 +1,13 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import react from "@vitejs/plugin-react";
 // `vitest` is included in the root package
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "vitest/config";
-import { getBuildConfig, wrapIIFE } from "../common/utils/vite-config.utils";
+import { getBuildConfig } from "../common/utils/vite-config.utils";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react, wrapIIFE],
+  plugins: [react],
   build: getBuildConfig(),
   test: {
     environment: "happy-dom",
