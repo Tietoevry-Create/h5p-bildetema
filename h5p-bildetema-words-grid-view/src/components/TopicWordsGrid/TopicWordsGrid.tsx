74f96dc5feef44a9aa8147f69d2bc8b33cd23683
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2022-09-05 14:20:20 +0200

    Remove show words hack

--- h5p-bildetema-words-grid-view/src/components/TopicWordsGrid/TopicWordsGrid.tsx
+++ h5p-bildetema-words-grid-view/src/components/TopicWordsGrid/TopicWordsGrid.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useContentId } from "use-h5p";
 import { Word as WordType } from "../../../../common/types/types";
 import { Word } from "../Word/Word";
 import styles from "./TopicWordsGrid.module.scss";
@@ -13,34 +12,10 @@ export const TopicWordsGrid: React.FC<TopicWordsGridProps> = ({
   words,
   showWrittenWords,
 }) => {
-  const [textVisible, setTextVisible] = React.useState(showWrittenWords);
-
-  const handleChange = (e: Event): void => {
-    setTextVisible((e.target as HTMLInputElement).checked);
-  };
-
-  const contentId = useContentId();
-
-  React.useEffect(() => {
-    // Hack: Navigating from topic -> words causes document.getElementById(`toggle-${contentId}`) to be null, if we not wait for one whole frame...
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        document
-          .getElementById(`toggle-${contentId}`)
-          ?.addEventListener("change", handleChange);
-      });
-    });
-    return () => {
-      document
-        .getElementById(`toggle-${contentId}`)
-        ?.removeEventListener("change", handleChange);
-    };
-  }, [contentId]);
-
   return (
     <div className={styles.topicgrid}>
       {words.map(word => (
-        <Word key={word.id} word={word} textVisible={textVisible} />
+        <Word key={word.id} word={word} textVisible={showWrittenWords} />
       ))}
     </div>
   );
