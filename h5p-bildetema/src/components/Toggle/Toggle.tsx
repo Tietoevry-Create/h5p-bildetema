a6826eeccd7af31af0d9193124514df415dffcdc
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2022-09-06 19:29:10 +0200

    Only show extra focus on tab, not click

--- h5p-bildetema/src/components/Toggle/Toggle.tsx
+++ h5p-bildetema/src/components/Toggle/Toggle.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Toggle.module.scss";
 
 type ToggleProps = {
@@ -13,8 +13,22 @@ export const Toggle: React.FC<ToggleProps> = ({
   label,
   id,
 }) => {
+  // Show orange focus when tab/keyboard is used, not on click
+  const [isFocused, setIsFocused] = useState(false);
+  const handleOnFocus = (showFocus: boolean): void => {
+    if (showFocus && !isFocused) {
+      setIsFocused(true);
+    }
+    if (!showFocus && isFocused) {
+      setIsFocused(false);
+    }
+  };
+
   return (
-    <label className={styles.container} htmlFor={id}>
+    <label
+      className={isFocused ? styles.containerFocused : styles.container}
+      htmlFor={id}
+    >
       {label && <span className={styles.label}>{label}</span>}
       <span className={styles.toggle}>
         <input
@@ -22,6 +36,8 @@ export const Toggle: React.FC<ToggleProps> = ({
           checked={checked}
           type="checkbox"
           onChange={e => handleChange(e.target.checked)}
+          onKeyUp={() => handleOnFocus(true)}
+          onBlur={() => handleOnFocus(false)}
         />
         <span className={styles.slider} />
       </span>
