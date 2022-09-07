9f3573e5d6a4c320de2eae923b17bbea57f7324f
Stian Haugland <stiahau@gmail.com>
2022-09-07 10:47:01 +0200

    Revert "remove `wrapIIFE`"
    This reverts commit 65ff52712882efb1b275601eb0dcfa7141572269.

--- common/utils/vite-config.utils.ts
+++ common/utils/vite-config.utils.ts
@@ -1,4 +1,26 @@
-import { BuildOptions } from "vite";
+import type { OutputAsset, OutputChunk } from "rollup";
+import { BuildOptions, PluginOption } from "vite";
+
+const isOutputChunk = (
+  chunkOrAsset: OutputChunk | OutputAsset,
+): chunkOrAsset is OutputChunk => {
+  return "code" in chunkOrAsset;
+};
+
+export const wrapIIFE = (): PluginOption => ({
+  name: "wrap-iife",
+  generateBundle(options, bundle) {
+    const chunks = Object.values(bundle);
+
+    for (let i = 0; i < chunks.length; i += 1) {
+      const chunk = chunks[i];
+      if (isOutputChunk(chunk)) {
+        // eslint-disable-next-line no-param-reassign
+        chunk.code = `(function(){${chunk.code}})()`;
+      }
+    }
+  },
+});
 
 export const getBuildConfig = (): BuildOptions => ({
   minify: "esbuild",
