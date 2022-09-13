4e47b64be7fad69bd6540d07f0a79de52cf4ca17
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2022-09-13 11:10:33 +0200

    Bump patch version

--- h5p-editor-bildetema-words-topic-image/src/library.ts
+++ h5p-editor-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5PEditor.BildetemaWordsTopicImage",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 22,
+  patchVersion: 23,
   runnable: 0,
   preloadedJs: [
     {
