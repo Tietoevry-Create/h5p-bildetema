9adffbdcb9600c690d6effa7a70a05c79551a040
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-09-20 15:52:41 +0200

    Make header smaller (#488)
    * Make header smaller
    
    * Bump patch version

--- h5p-bildetema/src/library.ts
+++ h5p-bildetema/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.Bildetema",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 76,
+  patchVersion: 77,
   runnable: 1,
   preloadedJs: [
     {
