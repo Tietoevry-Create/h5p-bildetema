7e923d8032762a24b9d2467bbd63bc947a8d4517
Stian Haugland <stian.haugland@tietoevry.com>
2022-09-26 14:38:17 +0200

    bump library

--- h5p-editor-bildetema-words-topic-image/src/library.ts
+++ h5p-editor-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5PEditor.BildetemaWordsTopicImage",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 24,
+  patchVersion: 25,
   runnable: 0,
   preloadedJs: [
     {
