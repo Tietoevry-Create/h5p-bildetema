bd6ac9ccffc60b75f97a2de4352b626f5235c0d6
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2022-09-27 11:03:34 +0200

    Bump patch version

--- h5p-bildetema/src/library.ts
+++ h5p-bildetema/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.Bildetema",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 79,
+  patchVersion: 80,
   runnable: 1,
   preloadedJs: [
     {
