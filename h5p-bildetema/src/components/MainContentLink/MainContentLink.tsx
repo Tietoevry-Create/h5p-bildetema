9de1aa9134d4795e05dc64719e8266bf4c9e8aef
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2022-09-29 12:32:05 +0200

    Only use css for focus

--- h5p-bildetema/src/components/MainContentLink/MainContentLink.tsx
+++ h5p-bildetema/src/components/MainContentLink/MainContentLink.tsx
@@ -1,32 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import { useL10ns } from "../../hooks/useL10n";
 import styles from "./MainContentLink.module.scss";
 
 export const MainContentLink: React.FC = () => {
   const { mainContentLink } = useL10ns("mainContentLink");
-  const [isFocused, setIsFocused] = useState<boolean>(false);
 
   const handleOnClick = (): void => {
-    setIsFocused(false);
-    const container: HTMLElement | null =
-      document.querySelector("#bildetemaMain");
+    const main: HTMLElement | null = document.querySelector("#bildetemaMain");
 
-    if (container) {
-      container.tabIndex = -1;
-      container.focus();
-      setTimeout(() => container.removeAttribute("tabindex"), 1000);
+    if (main) {
+      main.tabIndex = -1;
+      main.focus();
+      setTimeout(() => main.removeAttribute("tabindex"), 1000);
     }
   };
 
   return (
     <nav className={styles.nav}>
-      <button
-        type="button"
-        className={isFocused ? styles.linkFocused : styles.link}
-        onClick={handleOnClick}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-      >
+      <button type="button" className={styles.button} onClick={handleOnClick}>
         {mainContentLink}
       </button>
     </nav>
