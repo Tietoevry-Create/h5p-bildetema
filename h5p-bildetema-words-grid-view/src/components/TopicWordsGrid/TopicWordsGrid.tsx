14d92ec5d69d59c69dc79d591a1b886329b855c7
Stian Haugland <stian.haugland@tietoevry.com>
2022-09-30 15:11:00 +0200

    Revert "Use print component in h5p-words-grid"
    This reverts commit 61c4fe759faf37930364d1094ce690423b0d7882.

--- h5p-bildetema-words-grid-view/src/components/TopicWordsGrid/TopicWordsGrid.tsx
+++ h5p-bildetema-words-grid-view/src/components/TopicWordsGrid/TopicWordsGrid.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Word as WordType } from "../../../../common/types/types";
-import { PrintWords } from "../PrintWords/PrintWords";
 import { Word } from "../Word/Word";
 import styles from "./TopicWordsGrid.module.scss";
 
@@ -14,15 +13,10 @@ export const TopicWordsGrid: React.FC<TopicWordsGridProps> = ({
   showWrittenWords,
 }) => {
   return (
-    <>
-    <div className={styles.printWordsWrapper}>
-      <PrintWords words={words} showWrittenWords={showWrittenWords}/>
-    </div>
     <div className={styles.topicgrid}>
       {words.map(word => (
         <Word key={word.id} word={word} textVisible={showWrittenWords} />
-        ))}
+      ))}
     </div>
-    </>
   );
 };
