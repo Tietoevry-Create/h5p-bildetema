3026f577cfd93fe40cf062b9ec26f39a140b7f34
Sindre Bøyum <boyum@users.noreply.github.com>
2022-10-04 16:51:52 +0200

    feat: polyfill new JS functionality for older browsers (#549)

--- h5p-bildetema-words-topic-image/src/library.ts
+++ h5p-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.BildetemaTopicImageView",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 33,
+  patchVersion: 34,
   runnable: 1,
   preloadedJs: [
     {
