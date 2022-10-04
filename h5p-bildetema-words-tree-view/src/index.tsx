1ecdbe9bbd4cc15e7001d77f6dc0b5d88acff710
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-10-04 16:44:51 +0200

    fix: format

--- h5p-bildetema-words-tree-view/src/index.tsx
+++ h5p-bildetema-words-tree-view/src/index.tsx
@@ -1,5 +1,5 @@
 import { registerContentType } from "h5p-utils";
-import "../../common/polyfills"
+import "../../common/polyfills";
 import { H5PWrapper } from "./h5p/H5PWrapper";
 import "./index.scss";
 
