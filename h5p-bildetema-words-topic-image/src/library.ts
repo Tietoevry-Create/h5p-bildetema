cc45c4888a9f948f8bf7e77113978f1382fb1e6d
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-10-06 10:44:47 +0200

    feat: bump patch versions

--- h5p-bildetema-words-topic-image/src/library.ts
+++ h5p-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.BildetemaTopicImageView",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 35,
+  patchVersion: 36,
   runnable: 1,
   preloadedJs: [
     {
