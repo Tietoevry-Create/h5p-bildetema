4cf32ae21acd4fccff85c4d7f5157e5e5e06e2ee
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-10-12 13:37:43 +0200

    Hide WordPress skip to content link (#584)
    * Hide WordPress skip to content link
    
    * Change placement of new css
    
    * Bump patch version

--- h5p-bildetema/src/library.ts
+++ h5p-bildetema/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.Bildetema",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 101,
+  patchVersion: 102,
   runnable: 1,
   preloadedJs: [
     {
