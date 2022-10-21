41e0c6faaa5283dd7281e268102806fcbaa4434b
Yijun Pan Stautland <yijun@staut.land>
2022-10-21 20:30:01 +0200

    Bump library

--- h5p-bildetema-words-grid-view/src/library.ts
+++ h5p-bildetema-words-grid-view/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.BildetemaWordsGridView",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 43,
+  patchVersion: 44,
   runnable: 1,
   preloadedJs: [
     {
--- h5p-bildetema/src/library.ts
+++ h5p-bildetema/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.Bildetema",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 110,
+  patchVersion: 111,
   runnable: 1,
   preloadedJs: [
     {
