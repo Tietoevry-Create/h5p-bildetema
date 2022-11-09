6cf86d2e381cf61fa9e94ea642b4c60f010f9b44
Stian Haugland <stian.haugland@tietoevry.com>
2022-11-09 16:00:27 +0100

    fix storybook

--- h5p-editor-bildetema-words-topic-image/src/components/TopicChooser/TopicChooser.stories.tsx
+++ h5p-editor-bildetema-words-topic-image/src/components/TopicChooser/TopicChooser.stories.tsx
@@ -15,6 +15,7 @@ const baseTopic: Topic = {
   subTopics: [],
   words: new Map<LanguageCode, Word[]>(),
   labelTranslations: new Map<LanguageCode, Word>(),
+  onlyTopicImage: false,
   images: [
     {
       src: "https://images.unsplash.com/photo-1564349683136-77e08dba1ef7?ixlib=rb-1.2.1&w=640&q=80&fm=jpg&crop=entropy&cs=tinysrgb",
