94a6bc9a1d2083e08f6dc605b9f777542365bd56
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-11-16 13:12:09 +0100

    Align topic image words to left (rtl: right) (#707)
    * Align topic image words to left (rtl: right)
    
    * Bump patch version

--- h5p-bildetema-words-topic-image/src/library.ts
+++ h5p-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.BildetemaTopicImageView",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 52,
+  patchVersion: 53,
   runnable: 1,
   preloadedJs: [
     {
