d9fb55bf554da0ee63a0fc13c18ce840304ee913
Stian Haugland <stian.haugland@tietoevry.com>
2022-11-16 09:27:25 +0100

    bump library

--- h5p-bildetema/src/library.ts
+++ h5p-bildetema/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.Bildetema",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 128,
+  patchVersion: 129,
   runnable: 1,
   preloadedJs: [
     {
