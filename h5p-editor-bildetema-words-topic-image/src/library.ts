e3a3a187f28f2b22e1ba8e45ac1419dd4ebdc88b
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-11-29 11:35:13 +0100

    Make sure styling is not overrided by WordPress (#724)
    * Make sure styling is not overrided by WordPress
    
    * Prettier
    
    * Small fixes
    
    * Bump patch version

--- h5p-editor-bildetema-words-topic-image/src/library.ts
+++ h5p-editor-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5PEditor.BildetemaWordsTopicImage",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 48,
+  patchVersion: 49,
   runnable: 0,
   preloadedJs: [
     {
