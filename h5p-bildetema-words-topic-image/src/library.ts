379b861af2019897f019d0e279dcbaad9155d5bc
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-12-02 12:51:55 +0100

    Fix topic image img height (#741)
    * Fix topic image img height
    
    * Bump patch version
    
    * Change breakpoint for max-height

--- h5p-bildetema-words-topic-image/src/library.ts
+++ h5p-bildetema-words-topic-image/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.BildetemaTopicImageView",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 62,
+  patchVersion: 63,
   runnable: 1,
   preloadedJs: [
     {
