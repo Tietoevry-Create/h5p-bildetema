cbdae0183f612e58d7b127ec32335252d1cb010a
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2022-12-05 14:03:51 +0100

    Add Icelandic language file (#744)
    * Add Icelandic language file
    
    * Add translations from doc
    
    * Bump patch version

--- h5p-bildetema/src/library.ts
+++ h5p-bildetema/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.Bildetema",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 145,
+  patchVersion: 146,
   runnable: 1,
   preloadedJs: [
     {
