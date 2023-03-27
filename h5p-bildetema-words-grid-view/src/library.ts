2591972474bf333b79125c8bbdeb89bcdce258cc
Stian Haugland <stian.haugland@tietoevry.com>
2023-03-27 12:30:56 +0200

    bump libary

--- h5p-bildetema-words-grid-view/src/library.ts
+++ h5p-bildetema-words-grid-view/src/library.ts
@@ -5,7 +5,7 @@ export const library: Library = {
   machineName: "H5P.BildetemaWordsGridView",
   majorVersion: 1,
   minorVersion: 0,
-  patchVersion: 68,
+  patchVersion: 69,
   runnable: 1,
   preloadedJs: [
     {
