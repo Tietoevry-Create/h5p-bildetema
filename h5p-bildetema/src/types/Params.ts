06d757fb1869238b28b9778c1304ea57d1f60ce0
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-09-25 15:58:31 +0200

    refactor: remove unused type

--- h5p-bildetema/src/types/Params.ts
+++ h5p-bildetema/src/types/Params.ts
@@ -2,7 +2,6 @@ import { InferParamsFromSemantics } from "h5p-types";
 import semantics from "../../semantics.json";
 
 export type Params = InferParamsFromSemantics<typeof semantics>;
-export type TranslationKey = keyof Params["l10n"];
 
 // Import semantics into value space to ensure that `unplugin-json-dts`
 // generates the correct types.
