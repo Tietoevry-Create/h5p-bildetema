23afdb6d8de65e55cc8b4951cf03ea550f5911f4
Henriette Moe <35261194+henriettemoe@users.noreply.github.com>
2023-09-28 12:07:23 +0200

    fix: useLocalization => useTranslation (#1147)
    since useLocalization from use-h5p is deprecated

--- h5p-bildetema-words-topic-image/src/hooks/useL10n.ts
+++ h5p-bildetema-words-topic-image/src/hooks/useL10n.ts
@@ -1,6 +1,7 @@
-import { useLocalization } from "use-h5p";
+import { useTranslation } from "use-h5p";
 import { TranslationKey } from "../types/TranslationKey";
 
 export const useL10n = (translationKey: TranslationKey): string => {
-  return useLocalization<TranslationKey>(translationKey);
+  const { t } = useTranslation();
+  return t(translationKey);
 };
