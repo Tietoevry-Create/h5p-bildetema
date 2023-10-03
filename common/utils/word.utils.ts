252d2deaafc0211027a390650c089b0d886e7859
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2023-10-03 18:46:16 +0200

    update check

--- common/utils/word.utils.ts
+++ common/utils/word.utils.ts
@@ -5,16 +5,15 @@ export const extractWordLabel = (
   includeArticle: boolean,
 ): string => {
   const isTopicWord = "label" in word;
-  const isWord = "labels" in word;
-  if (!isTopicWord && !isWord) {
-    // word has not been updated yet
-    return "";
-  }
   if (isTopicWord) {
     const articleInWord = "article" in word;
     return articleInWord && includeArticle
       ? `${word.article as string} ${word.label as string}`
       : (word.label as string);
+  }
+  if (!word.labels) {
+    // Word has not been updated yet or is missing labels
+    return "";
   }
   return word.labels
     .map(el =>
@@ -27,6 +26,6 @@ export const wordsIncludesArticles = (words: Word[]): boolean => {
   return words.some(word => {
     const isTopicWord = "article" in word;
     if (isTopicWord) return !!(word.article as string);
-    return word.labels.some(el => el.article);
+    return word.labels?.some(el => el.article);
   });
 };
