cb3bbe8b1c0331da45f6f072e4c873e881520eaa
Stian Haugland <stian.haugland@tietoevry.com>
2024-03-06 15:02:07 +0100

    format

--- common/utils/searchResults.utils.ts
+++ common/utils/searchResults.utils.ts
@@ -3,7 +3,9 @@ import { distance, closest } from "fastest-levenshtein";
 import { LanguageCode } from "../types/LanguageCode";
 
 const getLabelsArray = (labels: Labels): string[] => {
-  return labels.map(el => el.label.split("/").map(l => l.trim().toLowerCase())).flat();
+  return labels
+    .map(el => el.label.split("/").map(l => l.trim().toLowerCase()))
+    .flat();
 };
 
 const findLabelClosestToSearch = (search: string, labels: Labels): string => {
@@ -42,7 +44,7 @@ export const sortSearchByTopic = (results: NewWord[]): NewWord[] => {
 
 const getSearchPositionValue = (search: string, labels: Labels): number => {
   const equal = 0;
-  const equalWithin = 1
+  const equalWithin = 1;
   const endsWith = 2;
   const startsWith = 3;
   const contains = 5;
@@ -52,7 +54,8 @@ const getSearchPositionValue = (search: string, labels: Labels): number => {
   const labelsSplitBySpace = labelsArray.join(" ").split(" ");
   if (labelsArray.includes(lowerCaseSearch)) return equal;
   if (labelsSplitBySpace.includes(lowerCaseSearch)) return equalWithin;
-  if (labelsArray.filter(l => l.endsWith(lowerCaseSearch)).length > 0) return endsWith;
+  if (labelsArray.filter(l => l.endsWith(lowerCaseSearch)).length > 0)
+    return endsWith;
   if (labelsArray.filter(l => l.startsWith(lowerCaseSearch)).length > 0)
     return startsWith;
   return contains;
