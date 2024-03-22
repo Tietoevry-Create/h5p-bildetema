36bc85cd27339f2f7e3e2eea26c5d5e2914c6b39
Stian Haugland <stian.haugland@tietoevry.com>
2024-03-22 12:04:05 +0100

    Auto focus on search, and blur on enterpress

--- h5p-bildetema/src/components/SearchPage/SearchInput/SearchInput.tsx
+++ h5p-bildetema/src/components/SearchPage/SearchInput/SearchInput.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { SearchIcon } from "../../Icons/Icons";
 import styles from "./SearchInput.module.scss";
 
@@ -6,13 +7,25 @@ export type SearchInputProps = {
   search: string;
 };
 
+
 const SearchInput = ({
   handleSearch,
   search,
 }: SearchInputProps): JSX.Element => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {  
+    ref.current?.focus()
+  }, [])
+
+  const handleEnter = (): void => {
+    ref.current?.blur();
+  }
+
   return (
     <div className={`${styles.searchInputWrapper}`}>
       <input
+        ref={ref}
         // TODO needs a label
         id={styles.searchInput}
         // TODO fix static Søk value
@@ -20,6 +33,7 @@ const SearchInput = ({
         value={search}
         autoComplete="off"
         onChange={e => handleSearch(e.target.value)}
+        onKeyDown={e => e.key === "Enter" && handleEnter()}
       />
       <span className={styles.searchInputIcon}>
         <SearchIcon width={20} height={20} />
