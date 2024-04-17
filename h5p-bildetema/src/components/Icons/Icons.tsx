044e823379ddbfa8b206dbdf8d23830d70b97931
Stian Haugland <stian.haugland@tietoevry.com>
2024-04-17 10:04:09 +0200

    fix feedback

--- h5p-bildetema/src/components/Icons/Icons.tsx
+++ h5p-bildetema/src/components/Icons/Icons.tsx
@@ -255,21 +255,14 @@ export const LanguageIcon: FC<
   </svg>
 );
 
-export const SearchIcon: FC<IconProps & IconSizeProps & IconTransformProps> = ({
-  iconColor,
-  width = 16,
-  height = 16,
-  transform,
-  transformOrigin,
-}) => (
+export const SearchIcon: FC = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    height={height}
-    width={width}
-    fill="currentColor"
-    viewBox="0 0 16 16"
+    height="24"
+    viewBox="0 -960 960 960"
+    width="24"
   >
-    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+    <path d="M380-320q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l224 224q11 11 11 28t-11 28q-11 11-28 11t-28-11L532-372q-30 24-69 38t-83 14Zm0-80q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
   </svg>
 );
 
