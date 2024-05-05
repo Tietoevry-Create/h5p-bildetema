e0600293e0161fef7a476fd2f9a7ca3fc1415bba
Sindre Bøyum <sindre.boyum@tietoevry.com>
2024-05-05 10:01:12 +0200

    build: use esm mode for vite

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,8 +1,8 @@
 import { defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
-  "./h5p-bildetema-words-topic-image/vite.config.ts",
-  "./h5p-bildetema/vite.config.ts",
-  "./h5p-bildetema-words-grid-view/vite.config.ts",
-  "./h5p-editor-bildetema-words-topic-image/vite.config.ts",
+  "./h5p-bildetema-words-topic-image/vite.config.mts",
+  "./h5p-bildetema/vite.config.mts",
+  "./h5p-bildetema-words-grid-view/vite.config.mts",
+  "./h5p-editor-bildetema-words-topic-image/vite.config.mts",
 ]);
