f29d48b61dd4c3ebed9d01b366cc1ffc66ab0dee
Sindre Haugland Bøyum <sindre.haugland.boyum@dnb.no>
2024-06-10 16:29:13 +0200

    build: generate types for semantics on build

--- h5p-bildetema/src/types/Params.ts
+++ h5p-bildetema/src/types/Params.ts
@@ -3,7 +3,3 @@ import semantics from "../../semantics.json";
 
 export type Params = InferParamsFromSemantics<typeof semantics>;
 
-// Import semantics into value space to ensure that `unplugin-json-dts`
-// generates the correct types.
-// eslint-disable-next-line no-unused-expressions
-semantics;
