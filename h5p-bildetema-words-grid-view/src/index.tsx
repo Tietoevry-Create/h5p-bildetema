9974817cb2e9abb86bde42543900093d25fafd82
Sindre Bøyum <boyum@users.noreply.github.com>
2024-06-11 06:25:19 +0200

    build: generate types for semantics on build (#1288)

--- h5p-bildetema-words-grid-view/src/index.tsx
+++ h5p-bildetema-words-grid-view/src/index.tsx
@@ -1,6 +1,12 @@
-import { registerContentType } from "h5p-utils";
 import "common/polyfills";
+import { registerContentType } from "h5p-utils";
+import semantics from "../semantics.json";
 import { H5PWrapper } from "./h5p/H5PWrapper";
 import "./index.scss";
 
 registerContentType("BildetemaWordsGridView", H5PWrapper);
+
+// Import semantics into value space to ensure that `unplugin-json-dts`
+// generates the correct types.
+// eslint-disable-next-line no-unused-expressions
+semantics;
