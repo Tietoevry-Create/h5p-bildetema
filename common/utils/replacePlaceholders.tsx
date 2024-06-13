20ab4a2304a8187c1ebd440dda3317f001b8c804
Sander Nicolaysen <sander.nicolaysen@tietoevry.com>
2024-06-13 08:14:24 +0200

    fix error

--- common/utils/replacePlaceholders.tsx
+++ common/utils/replacePlaceholders.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from "react";
 
 export const replacePlaceholders = (
   text: string,
