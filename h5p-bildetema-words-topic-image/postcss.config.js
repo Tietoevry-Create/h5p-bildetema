a94376792b480cf60b6b2d58fbde55ad549dc190
Sindre Haugland Bøyum <sindre.haugland.boyum@dnb.no>
2024-10-10 14:33:44 +0200

    chore: fix lint

--- h5p-bildetema-words-topic-image/postcss.config.js
+++ h5p-bildetema-words-topic-image/postcss.config.js
@@ -1,7 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+ 
 
 import { commonPostCSSConfig as commonConfig } from "common/utils/postcss-config/postcss-config.utils.js";
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default () => ({
   ...commonConfig,
 });
