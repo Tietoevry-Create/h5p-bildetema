616201559d4ae08580507f0dd873937a8ba245b1
Sindre Haugland Bøyum <sindre.haugland.boyum@dnb.no>
2024-10-10 14:35:32 +0200

    chore: fix format

--- h5p-bildetema-words-topic-image/postcss.config.js
+++ h5p-bildetema-words-topic-image/postcss.config.js
@@ -1,5 +1,3 @@
- 
-
 import { commonPostCSSConfig as commonConfig } from "common/utils/postcss-config/postcss-config.utils.js";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
