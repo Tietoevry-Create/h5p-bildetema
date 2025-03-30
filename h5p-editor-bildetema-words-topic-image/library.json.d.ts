f67588fab708ac6899baffb6f2f04ced1329e3e9
Sindre Haugland Bøyum <sindre.haugland.boyum@dnb.no>
2025-03-30 17:46:39 +0200

    chore: update Vite to v6

--- h5p-editor-bildetema-words-topic-image/library.json.d.ts
+++ h5p-editor-bildetema-words-topic-image/library.json.d.ts
@@ -4,25 +4,17 @@ export const majorVersion : 1;
 export const minorVersion : 0;
 export const patchVersion : 115;
 export const runnable : 0;
-export const preloadedJs : [
-	{
-		path: "dist/bundle.js"
-	}
-];
-export const preloadedCss : [
-	{
-		path: "dist/main.css"
-	}
-];
+export const preloadedJs : [{"path":"dist/bundle.js"}];
+export const preloadedCss : [{"path":"dist/main.css"}];
 declare const $defaultExport: {
-	title: typeof title,
-	machineName: typeof machineName,
-	majorVersion: typeof majorVersion,
-	minorVersion: typeof minorVersion,
-	patchVersion: typeof patchVersion,
-	runnable: typeof runnable,
-	preloadedJs: typeof preloadedJs,
-	preloadedCss: typeof preloadedCss
+  title: typeof title,
+  machineName: typeof machineName,
+  majorVersion: typeof majorVersion,
+  minorVersion: typeof minorVersion,
+  patchVersion: typeof patchVersion,
+  runnable: typeof runnable,
+  preloadedJs: typeof preloadedJs,
+  preloadedCss: typeof preloadedCss,
 };
 
 export default $defaultExport;
