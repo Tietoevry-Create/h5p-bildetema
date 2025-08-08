cad427b7f239c93c2b5aede31667ec521cd31f7a
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2025-08-08 15:25:23 +0200

    bump patch versions

--- h5p-bildetema-words-grid-view/library.json.d.ts
+++ h5p-bildetema-words-grid-view/library.json.d.ts
@@ -2,7 +2,7 @@ export const title : "H5P Bildetema Words Grid View";
 export const machineName : "H5P.BildetemaWordsGridView";
 export const majorVersion : 1;
 export const minorVersion : 0;
-export const patchVersion : 130;
+export const patchVersion : 131;
 export const runnable : 1;
 export const preloadedJs : [
 	{
