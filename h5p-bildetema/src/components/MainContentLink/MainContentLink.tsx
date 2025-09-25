254d37223c2b78858ab530a6ef2484a189b7e06d
Henriette Rudi Moe <henriette.moe@tietoevry.com>
2025-09-25 11:23:15 +0200

    fix: use HashLink for go to main content link

--- h5p-bildetema/src/components/MainContentLink/MainContentLink.tsx
+++ h5p-bildetema/src/components/MainContentLink/MainContentLink.tsx
@@ -1,26 +1,16 @@
 import { FC } from "react";
+import { HashLink } from "react-router-hash-link";
 import { useL10ns } from "../../hooks/useL10n";
 import styles from "./MainContentLink.module.scss";
 
 export const MainContentLink: FC = () => {
   const { mainContentLink } = useL10ns("mainContentLink");
 
-  // Hand craft `<a href="#bildetemaMain">` because we're using HashRouter
-  // and can't use the builtin functionality.
-  const handleOnClick = (): void => {
-    const main: HTMLElement | null = document.querySelector("#bildetemaMain");
-
-    if (main) {
-      main.tabIndex = -1;
-      main.focus();
-    }
-  };
-
   return (
     <nav className={styles.nav}>
-      <button type="button" className={styles.button} onClick={handleOnClick}>
+      <HashLink className={styles.link} to="#bildetemaMain">
         {mainContentLink}
-      </button>
+      </HashLink>
     </nav>
   );
 };
